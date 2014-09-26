fce9fb2554ea0a987246498006186e489e49af32
Thiago Cangussu <cangussu.thg@gmail.com>
2014-09-26 10:30:13 -0300

    Added flask Dockerfile

---
+++ python/flask/manage.py
@@ -0,0 +1,15 @@
+# manage.py
+
+from flask.ext.script import Manager
+from app import app
+
+manager = Manager(app)
+
+"""
+@manager.command
+def hello():
+    print "hello"
+"""
+
+if __name__ == "__main__":
+    manager.run()
