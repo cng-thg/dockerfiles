0966843699a9206a6507098f33d2163cf9d8cbfa
Thiago Cangussu <cng.thg@gmail.com>
2014-10-14 11:11:01 -0300

    [GENERIC]   - removed wp.conf   - removed wp.sh   - Not use them

--- wordpress/wp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export DB_HOST=$(serf members -tag role=${DB_ROLE:-"mysql"} | grep alive | awk {'print $2'} | cut -d':' -f1)
-
-DBHOST=$(serf members -tag role=${DB_ROLE:-"mysql"} | grep alive | awk {'print $2'} | cut -d':' -f1)
-
-export DB=mysql
-
-exec serf monitor
