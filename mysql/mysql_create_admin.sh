758370d470e979c5f0ff530837d99f6bebe43b99
Thiago Cangussu <cng.thg@gmail.com>
2014-10-14 10:43:16 -0300

    [FIX]   - check if MYSQL_ADMIN_USER was given, if not, exits script   - removed mysql_admin_user.pid   - creates user through grant statement   - removed unused echos

--- mysql/mysql_create_admin.sh
+++ mysql/mysql_create_admin.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+###############################################################################
+# Create MySQL admin user
+###############################################################################
 
-if [ -f /var/run/mysql_admin_user.pid ]; then
-  echo "MySQL 'admin' user already created!"
-  exit 0
-fi
+echo "Creating MySQL admin user"
+## Start MySQL service
+echo "Starting MySQL service"
+/usr/bin/mysqld_safe > /dev/null 2>&1 &
 
-
-# Start MySQL service
-service mysql start
-
+# Check if mysql service is up
 RET=1
 
 while [[ RET -ne 0 ]]; do
@@ -21,34 +21,30 @@ while [[ RET -ne 0 ]]; do
   RET=$?
 done
 
-# Getting host ip
+## Getting host ip
 HOST_IP=${HOST_IP:-$(ip route | cut -d " " -f 3 | head -n 1)}
 
-# Set MySQL admin user (default = admin with blank password)
-MYSQL_ADMIN_USER=${MYSQL_ADMIN_USER:-"admin"}
-MYSQL_ADMIN_PASSWD=${MYSQL_ADMIN_PASSWD:-""}
+## Set MySQL admin user
+MYSQL_ADMIN_USER=${MYSQL_ADMIN_USER}
+MYSQL_ADMIN_PASSWD=${MYSQL_ADMIN_PASSWD}
 
+# If admin user is empty, exits script.
+if [[ -z "$MYSQL_ADMIN_USER" ]]; then
+  echo "No admin name given, exiting."
+  exit 1
+fi
 
-echo "=> Creating MySQL user ${MYSQL_ADMIN_USER} with ${MYSQL_ADMIN_PASSWD} password"
-
-# Crete user at docker host and grant privileges
-mysql -uroot -e "CREATE USER ${MYSQL_ADMIN_USER}@${HOST_IP} IDENTIFIED BY '${MYSQL_ADMIN_PASSWD}'"
+## Crete admin user
+# MySQL admin user is created for remote admin tasks. For safety, script uses
+# docker host ip, so you need to log on docker host and then performs admin
+# tasks via mysql -h <CONTAINER_IP> -u <MYSQL_ADMIN_USER} -p<PASSWD>
+#mysql -uroot -e "CREATE USER IF NOT EXISTS ${MYSQL_ADMIN_USER}@${HOST_IP} IDENTIFIED BY '${MYSQL_ADMIN_PASSWD}'"
+echo "Creating ${MYSQL_ADMIN_USER}"
 
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_ADMIN_USER}@${HOST_IP} IDENTIFIED BY '${MYSQL_ADMIN_PASSWD}' WITH GRANT OPTION"
 
 mysql -uroot -e "FLUSH PRIVILEGES"
 
-echo "=> Done!"
-echo "========================================================================"
-echo "You can now connect to this MySQL Server using:"
-echo ""
-echo " mysql -u$MYSQL_ADMIN_USER -p$MYSQL_ADMIN_PASSWD -h<host> -P<port>"
-echo ""
-echo "Please remember to change the above password as soon as possible!"
-echo "MySQL user 'root' has no password but only allows local connections"
-echo "========================================================================"
-
-touch /var/run/mysql_admin_user.pid
-
-# Stop MySQL service
+## Stop MySQL service
+echo "Stopping MySQL service"
 mysqladmin -uroot shutdown
