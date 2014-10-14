bf542e252f67d5ee932a7acda2708f254fa3aff8
Thiago Cangussu <cng.thg@gmail.com>
2014-10-14 10:46:45 -0300

    [FEATURE]   - check if mysql_create_admin.sh exists, if yes, execute it.   - check if mysql_create_db.sh exists, if yes, execute it.   - check for supervisord and execute it.

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
+###############################################################################
+# Start MySQL service
+###############################################################################
 
-if [ ! -f /var/run/mysql_admin_user.pid ]; then
-  /opt/mysql_create_admin.sh
+CREATE_ADMIN=/opt/mysql_create_admin.sh
+CREATE_USER=/opt/mysql_create_user.sh
+CREATE_DB=/opt/mysql_create_db.sh
+
+## Create admin user
+if [ -e "$CREATE_ADMIN" ]; then
+  $CREATE_ADMIN
 fi
 
-exec mysqld_safe
+## Create normal user
+#if [ -e "$CREATE_USER"]; then
+#  $CREATE_USER
+#fi
+
+## Create database
+if [ -e "$CREATE_DB" ]; then
+  $CREATE_DB
+fi
+
+## Start supervisor
+if [ $(which supervisord) ]; then
+  supervisord -n -c /etc/supervisor/supervisord.conf
+fi
