1bc049a71a6542091eed596300dd60567183715f
Thiago Cangussu <cng.thg@gmail.com>
2014-10-14 10:53:03 -0300

    [FIX]   - Removed script to create user

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -4,18 +4,12 @@
 ###############################################################################
 
 CREATE_ADMIN=/opt/mysql_create_admin.sh
-CREATE_USER=/opt/mysql_create_user.sh
 CREATE_DB=/opt/mysql_create_db.sh
 
 ## Create admin user
 if [ -e "$CREATE_ADMIN" ]; then
   $CREATE_ADMIN
 fi
-
-## Create normal user
-#if [ -e "$CREATE_USER"]; then
-#  $CREATE_USER
-#fi
 
 ## Create database
 if [ -e "$CREATE_DB" ]; then
